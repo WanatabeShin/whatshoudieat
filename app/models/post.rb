f7cf54c76bfc36f38e89ce22ca7762df01ee30d9
Kevin Antonius <kevin.antonius95@gmail.com>
2018-03-02 05:38:07 +0000

    added feature Post photo

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,6 @@ class Post < ApplicationRecord
   def to_s
       title
   end
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   def set_default_role
     self.role ||= :user
   end
+  
+  # has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }
+  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
